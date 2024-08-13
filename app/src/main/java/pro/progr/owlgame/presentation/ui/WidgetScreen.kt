1b019e4e6b2f081ae617abd0dbe9a742a00a7afa
Anastasiia Makarenko <irishmoth@yandex.ru>
2024-08-13 19:39:40 +0300

    Hello

---
+++ app/src/main/java/pro/progr/owlgame/presentation/ui/WidgetScreen.kt
@@ -0,0 +1,9 @@
+package pro.progr.owlgame.presentation.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WidgetScreen() {
+    Text(text = "Hello, it is working!")
+}
