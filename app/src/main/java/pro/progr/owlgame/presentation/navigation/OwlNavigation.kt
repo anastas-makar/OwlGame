7459ac6c0e8aa8a953ea31ac98aa1212e2d4c17e
Anastasiia Makarenko <irishmoth@yandex.ru>
2024-09-05 23:21:04 +0300

    Все окли смысл

--- app/src/main/java/pro/progr/owlgame/presentation/navigation/OwlNavigation.kt
+++ app/src/main/java/pro/progr/owlgame/presentation/navigation/OwlNavigation.kt
@@ -1,8 +1,8 @@
 package pro.progr.owlgame.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun AppNavigation() {
-
+fun OwlNavigation() {
 }
--- app/src/main/java/pro/progr/owlgame/presentation/ui/WidgetScreen.kt
+++ app/src/main/java/pro/progr/owlgame/presentation/ui/WidgetScreen.kt
@@ -1,9 +1,15 @@
 package pro.progr.owlgame.presentation.ui
 
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 
 @Composable
-fun WidgetScreen() {
-    Text(text = "Hello, it is working!")
+fun WidgetScreen(navController: NavHostController) {
+    Button(onClick = {
+        navController.navigate("module_main")
+    }) {
+        Text("К спасению совы")
+    }
 }
