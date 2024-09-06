77b040486cd1c2c33812bb83dc87682039a6cb98
Anastasiia Makarenko <irishmoth@yandex.ru>
2024-09-06 17:20:27 +0300

    Исправляю навигацию (отдельный контроллер для модуля)

--- app/src/main/java/pro/progr/owlgame/presentation/navigation/OwlNavigation.kt
+++ app/src/main/java/pro/progr/owlgame/presentation/navigation/OwlNavigation.kt
@@ -1,13 +1,15 @@
 package pro.progr.owlgame.presentation.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import pro.progr.owlgame.presentation.ui.TownsListScreen
 
 @Composable
-fun OwlNavigation(navController: NavHostController) {
+fun OwlNavigation() {
+    val navController = rememberNavController()
+
     NavHost(navController = navController, startDestination = "towns") {
         composable("towns") {
             TownsListScreen()
