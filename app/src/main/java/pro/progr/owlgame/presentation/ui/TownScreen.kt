d1e8faea94a00517dfcf0cd7170d2d197fab13c7
Anastasiia Makarenko <irishmoth@yandex.ru>
2024-09-11 21:08:42 +0300

    Начинаю разрабатывать страницу города

---
+++ app/src/main/java/pro/progr/owlgame/presentation/ui/TownScreen.kt
@@ -0,0 +1,31 @@
+package pro.progr.owlgame.presentation.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import pro.progr.owlgame.presentation.viewmodel.TownViewModel
+
+@Composable
+fun TownScreen(navController: NavHostController, townViewModel: TownViewModel) {
+    Scaffold(
+        topBar = {
+            Box(modifier = Modifier.statusBarsPadding()) {
+                TownBar(navController, townViewModel)
+            }
+        },
+        content = { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding).fillMaxSize()) {
+
+                Text("Map of town")
+            }
+
+
+        }
+    )
+}
