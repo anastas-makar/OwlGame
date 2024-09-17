0ea4bd411d13f8e42247d8547f13e340b626a6ef
Anastasiia Makarenko <irishmoth@yandex.ru>
2024-09-17 23:20:40 +0300

    фикс

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,9 +59,9 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.navigation:navigation-compose:2.8.0")
 
-    implementation "androidx.work:work-runtime-ktx:2.9.1"
-    implementation 'com.google.dagger:dagger:2.48'
-    ksp 'com.google.dagger:dagger-compiler:2.48'
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
+    implementation("com.google.dagger:dagger:2.48")
+    ksp("com.google.dagger:dagger-compiler:2.48")
 }
 
 publishing {
