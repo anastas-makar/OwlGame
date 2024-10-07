52d609129e3089a91b2db457e2fb36b1f6c3acf3
Anastasiia Makarenko <irishmoth@yandex.ru>
2024-10-07 23:30:07 +0300

    Добавляю retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+
     implementation("androidx.compose.ui:ui:1.7.2")
     implementation("androidx.compose.ui:ui-tooling:1.7.2")
     implementation("androidx.compose.material:material:1.7.2")
