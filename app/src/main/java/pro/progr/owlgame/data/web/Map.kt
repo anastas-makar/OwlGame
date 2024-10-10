8149c680ba9a2dfdd254703c70294f2ff670f3e2
Anastasiia Makarenko <irishmoth@yandex.ru>
2024-10-10 23:15:15 +0300

    Начало настройки

---
+++ app/src/main/java/pro/progr/owlgame/data/web/Map.kt
@@ -0,0 +1,7 @@
+package pro.progr.owlgame.data.web
+
+data class Map(
+    val id: String,
+    val name: String,
+    val imageUrl: String
+)
---
+++ app/src/main/java/pro/progr/owlgame/data/web/MapApiService.kt
@@ -0,0 +1,12 @@
+package pro.progr.owlgame.data.web
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MapApiService {
+    @GET("maps")
+    suspend fun getMaps(
+        @Query("apiKey") apiKey: String = ""//TODO: BuildConfig.API_KEY
+    ): Response<List<Map>>
+}
