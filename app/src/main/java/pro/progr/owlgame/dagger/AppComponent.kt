433a1f7ae2b06b5ccdcbe972189616717481dd6b
Anastasiia Makarenko <irishmoth@yandex.ru>
2024-10-16 23:50:52 +0300

    Подключаю NetworkModule

--- app/src/main/java/pro/progr/owlgame/dagger/AppComponent.kt
+++ app/src/main/java/pro/progr/owlgame/dagger/AppComponent.kt
@@ -1,10 +1,11 @@
 package pro.progr.owlgame.dagger
 
 import dagger.Component
+import pro.progr.owlgame.data.dagger.NetworkModule
 import pro.progr.owlgame.presentation.viewmodel.dagger.TownViewModelFactory
 import pro.progr.owlgame.presentation.viewmodel.dagger.ViewModelFactory
 
-@Component(modules = [TownModule::class])
+@Component(modules = [TownModule::class, NetworkModule::class])
 interface AppComponent {
     fun townViewModelFactory(): TownViewModelFactory
 
