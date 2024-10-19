48da2c9d4a7c12ff4623e3d1f5b71f15ceada193
Anastasiia Makarenko <irishmoth@yandex.ru>
2024-10-19 21:58:19 +0300

    Подключаю coil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,6 +93,8 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
+
+    implementation("io.coil-kt:coil-compose:2.7.0")
 }
 
 publishing {
