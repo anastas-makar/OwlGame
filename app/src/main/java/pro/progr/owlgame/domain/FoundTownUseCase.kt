4762b5b4a05a7ad56276bbbcaf4abbc8626cec15
Anastasiia Makarenko <irishmoth@yandex.ru>
2024-11-22 23:47:20 +0300

    маленький фикс

--- app/src/main/java/pro/progr/owlgame/domain/FoundTownUseCase.kt
+++ app/src/main/java/pro/progr/owlgame/domain/FoundTownUseCase.kt
@@ -3,8 +3,9 @@ package pro.progr.owlgame.domain
 import pro.progr.owlgame.data.db.Town
 import pro.progr.owlgame.data.repository.TownRepository
 import pro.progr.owlgame.data.web.Map
+import javax.inject.Inject
 
-class FoundTownUseCase(val townRepository: TownRepository) {
+class FoundTownUseCase @Inject constructor(val townRepository: TownRepository) {
     operator fun invoke(map: Map, name: String) {
         //todo: сохранение карты
         val mapId = map.id
