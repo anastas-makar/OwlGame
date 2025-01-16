0b78dcdbb7caead7dc6918f4c1db58f8436670d8
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-01-16 23:47:35 +0300

    Строящееся здание будет отображаться где-то под картой

--- app/src/main/java/pro/progr/owlgame/presentation/ui/MapScreen.kt
+++ app/src/main/java/pro/progr/owlgame/presentation/ui/MapScreen.kt
@@ -1,13 +1,19 @@
 package pro.progr.owlgame.presentation.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedTextField
@@ -22,8 +28,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import pro.progr.owlgame.data.web.Map
 import pro.progr.owlgame.presentation.viewmodel.MapViewModel
 import pro.progr.owlgame.presentation.viewmodel.dagger.DaggerMapViewModel
@@ -98,6 +108,39 @@ fun MapScreen(
                     }
 
                     DraggableImages(map, mapViewModel)
+
+                    mapViewModel.selectedBuilding.value?.let {newHouse ->
+                        LazyVerticalGrid(
+                            columns = GridCells.Fixed(2),
+                            modifier = Modifier.fillMaxSize(),
+                            contentPadding = PaddingValues(8.dp),
+                            verticalArrangement = Arrangement.spacedBy(8.dp),
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            itemsIndexed(listOf(newHouse)) { _, building ->
+                                Box(
+                                    modifier = Modifier
+                                        .padding(30.dp)
+                                        .fillMaxSize()
+                                ) {
+                                    AsyncImage(
+                                        model = ImageRequest.Builder(LocalContext.current)
+                                            .data(building.imageResource)
+                                            .build(),
+                                        contentDescription = null,
+                                        contentScale = ContentScale.FillWidth,
+                                        modifier = Modifier
+                                            .fillMaxSize()
+                                            .clickable {
+                                                mapViewModel.selectHouseState.value = false
+                                                mapViewModel.selectedBuilding.value = building
+                                                mapViewModel.newHouseState.value = true
+                                            }
+                                    )
+                                }
+                            }
+                        }
+                    }
                 }
 
                 if (foundTown.value) {
