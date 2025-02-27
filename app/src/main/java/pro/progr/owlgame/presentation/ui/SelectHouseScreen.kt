250f4a9e3f18e0eadf30ef845c20a0ec70d659f8
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-02-27 22:13:21 +0300

    Добавляю цену для домиков

--- app/src/main/java/pro/progr/owlgame/presentation/ui/SelectHouseScreen.kt
+++ app/src/main/java/pro/progr/owlgame/presentation/ui/SelectHouseScreen.kt
@@ -4,21 +4,32 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import pro.progr.owlgame.R
 import pro.progr.owlgame.presentation.ui.model.BuildingModel
 import pro.progr.owlgame.presentation.viewmodel.MapViewModel
 
@@ -44,18 +55,11 @@ fun SelectHouseScreen(mapViewModel: MapViewModel, diamondBalance : State<Int>) {
         ) {
             itemsIndexed(buildingsState.value) { _, building ->
 
-                Box(
+                Column(
                     modifier = Modifier
                         .padding(30.dp)
-                        .fillMaxSize()
-                ) {
-                    AsyncImage(
-                        model = building.imageUrl,
-                        contentDescription = null,
-                        contentScale = ContentScale.FillWidth,
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .clickable {
+                        .clickable {
+                            if (diamondBalance.value >= building.price) {
                                 mapViewModel.selectHouseState.value = false
                                 mapViewModel.selectedBuilding.value = BuildingModel(
                                     building.id,
@@ -63,9 +67,40 @@ fun SelectHouseScreen(mapViewModel: MapViewModel, diamondBalance : State<Int>) {
                                     building.imageUrl
                                 )
                                 mapViewModel.newHouseState.value = true
-                            },
-                        colorFilter = if (diamondBalance.value < building.price) ColorFilter.tint(Color.Gray) else null
+                            }
+                        }.background(color = Color.White, shape = RoundedCornerShape(2.dp))
+                        .fillMaxSize()
+                ) {
+                    AsyncImage(
+                        model = building.imageUrl,
+                        contentDescription = null,
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        //todo: он здесь не нужен, просто прикольно смотрится, надо убрать
+                        colorFilter = if (diamondBalance.value < building.price) ColorFilter.lighting(Color.White, Color.DarkGray) else null
                     )
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .wrapContentHeight()
+                            .align(Alignment.CenterHorizontally)
+                    ) {
+                        Text(
+                            text = " ◆ ${building.price} ",
+                            style = MaterialTheme.typography.body1
+                        )
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_diamond_bright),
+                            contentDescription = "Diamond",
+                            tint = if (diamondBalance.value < building.price) Color.DarkGray
+                                        else Color.Unspecified,
+                            modifier = Modifier.height(12.dp)
+                        )
+                    }
+
                 }
             }
         }
