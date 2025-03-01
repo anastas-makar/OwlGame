f60b5ccf850ce5b8bc76602c33d2729fde5129c7
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-03-01 21:05:34 +0300

    Животные: начало

---
+++ app/src/main/java/pro/progr/owlgame/data/db/Animal.kt
@@ -0,0 +1,13 @@
+package pro.progr.owlgame.data.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "animals")
+data class Animal(
+    @PrimaryKey
+    val id : String,
+    val name: String,
+    val status: AnimalStatus,
+    val buildingId: String?
+)
---
+++ app/src/main/java/pro/progr/owlgame/data/db/AnimalStatus.kt
@@ -0,0 +1,7 @@
+package pro.progr.owlgame.data.db
+
+enum class AnimalStatus {
+    SEARCHING,
+    PET,
+    GONE;
+}
