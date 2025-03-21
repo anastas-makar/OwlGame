3aca388a6720153d2e2a575e4d0e6cb2c9de231b
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-03-21 23:21:09 +0300

    Revert "Фикс подключения"
    This reverts commit 8e421dae6b58a2f2e0ce60609c5de00e9d912c14.

--- app/src/main/java/pro/progr/owlgame/dagger/AppComponent.kt
+++ app/src/main/java/pro/progr/owlgame/dagger/AppComponent.kt
@@ -9,7 +9,6 @@ import pro.progr.owlgame.presentation.viewmodel.dagger.MapViewModelFactory
 import pro.progr.owlgame.presentation.viewmodel.dagger.PouchViewModelFactory
 import pro.progr.owlgame.presentation.viewmodel.dagger.TownViewModelFactory
 import pro.progr.owlgame.presentation.viewmodel.dagger.ViewModelFactory
-import pro.progr.owlgame.worker.GameWorkerFactory
 import javax.inject.Singleton
 
 @Singleton
@@ -29,7 +28,7 @@ interface AppComponent {
 
     fun viewModelFactory() : ViewModelFactory
 
-    fun gameWorkerFactory() : GameWorkerFactory
+
 
     @Component.Builder
     interface Builder {
