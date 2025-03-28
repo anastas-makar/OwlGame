817d74e123e1800fedcccc7f7a6472227eac23f9
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-03-28 19:47:49 +0300

    AnimalApiService

---
+++ app/src/main/java/pro/progr/owlgame/data/web/AnimalApiService.kt
@@ -0,0 +1,13 @@
+package pro.progr.owlgame.data.web
+
+import pro.progr.owlgame.data.db.Animal
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface AnimalApiService {
+    @GET("animal")
+    suspend fun getAnimal(
+        @Query("apiKey") apiKey: String
+    ): Response<Animal>
+}
