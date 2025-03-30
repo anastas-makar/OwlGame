a44cced80723e250af56167e8205e3b5b841340b
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-03-30 14:20:42 +0300

    Фикс импорта

--- app/src/main/java/pro/progr/owlgame/worker/AnimalBuildingsWorker.kt
+++ app/src/main/java/pro/progr/owlgame/worker/AnimalBuildingsWorker.kt
@@ -12,6 +12,7 @@ import pro.progr.owlgame.domain.SearchAnimalsUseCase
 import java.time.LocalDate
 import java.time.format.TextStyle
 import java.util.Locale
+import pro.progr.owlgame.BuildConfig
 
 class AnimalBuildingsWorker(
     context: Context,
