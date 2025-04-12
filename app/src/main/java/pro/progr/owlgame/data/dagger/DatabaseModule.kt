5fd1e6b1e8b4e9661f04fbd89c6cdcc980c75ab9
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-04-12 16:18:55 +0300

    Использую deeplink

--- app/src/main/java/pro/progr/owlgame/data/dagger/DatabaseModule.kt
+++ app/src/main/java/pro/progr/owlgame/data/dagger/DatabaseModule.kt
@@ -1,7 +1,6 @@
 package pro.progr.owlgame.data.dagger
 
 import android.content.Context
-import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import pro.progr.owlgame.data.db.AnimalDao
--- app/src/main/java/pro/progr/owlgame/worker/AnimalBuildingsWorker.kt
+++ app/src/main/java/pro/progr/owlgame/worker/AnimalBuildingsWorker.kt
@@ -2,7 +2,10 @@ package pro.progr.owlgame.worker
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.work.CoroutineWorker
@@ -62,11 +65,24 @@ class AnimalBuildingsWorker(
         )
         notificationManager.createNotificationChannel(channel)
 
+        val deepLinkUri = Uri.parse("owlgame://animal/$animalId")
+        val intent = Intent(Intent.ACTION_VIEW, deepLinkUri).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+
         val notification = NotificationCompat.Builder(applicationContext, channelId)
             .setSmallIcon(R.drawable.test2)
             .setContentTitle("$animalName ищет дом")
             .setContentText("Нажми, чтобы узнать больше")
-            //.setContentIntent(pendingIntent)
+            .setContentIntent(pendingIntent)
             .setAutoCancel(true)
             .build()
 
