7cde9b5f275d1b4f9f94760b8dce68bbf6675f08
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-05-22 23:51:47 +0300

    М

--- app/src/main/java/pro/progr/owlgame/presentation/viewmodel/WidgetViewModel.kt
+++ app/src/main/java/pro/progr/owlgame/presentation/viewmodel/WidgetViewModel.kt
@@ -3,9 +3,23 @@ package pro.progr.owlgame.presentation.viewmodel
 import android.app.Application
 import android.content.Context
 import androidx.lifecycle.AndroidViewModel
+import pro.progr.owlgame.R
+import pro.progr.owlgame.presentation.ui.model.OwlMenuModel
 
 class WidgetViewModel(application: Application) : AndroidViewModel(application) {
 
     private val prefs = application.getSharedPreferences("animal_search_prefs", Context.MODE_PRIVATE)
 
+    val menuList = listOf(
+        OwlMenuModel(
+            text = "Карты",
+            navigateTo = "owl_navigation",
+            imageResource = R.drawable.test1
+        ),
+        OwlMenuModel(
+            text = "Открыть мешочек",
+            navigateTo = "owl_navigation/pouch",
+            imageResource = R.drawable.pouch
+        )
+    )
 }
