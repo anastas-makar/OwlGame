af290d7d21b0115b0f6fd0cd30212c7031800c37
Anastasiia Makarenko <irishmoth@yandex.ru>
2026-02-14 07:14:49 +0300

    Индекс добавляю

--- app/src/main/java/pro/progr/owlgame/data/db/SupplyToReceipt.kt
+++ app/src/main/java/pro/progr/owlgame/data/db/SupplyToReceipt.kt
@@ -1,9 +1,11 @@
 package pro.progr.owlgame.data.db
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "supply_to_receipt")
+@Entity(tableName = "supply_to_receipt",
+    indices = [Index(value = ["receiptId", "supplyId"], unique = true)])
 data class SupplyToReceipt (
     @PrimaryKey
     val id : String,
