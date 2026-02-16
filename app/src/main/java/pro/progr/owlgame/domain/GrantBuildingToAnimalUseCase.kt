7726b9bc982e7719ae34c221cd95bda3868f4e9b
Anastasiia Makarenko <irishmoth@yandex.ru>
2026-02-16 09:59:54 +0300

    фикс обновления prefs

--- app/src/main/java/pro/progr/owlgame/domain/GrantBuildingToAnimalUseCase.kt
+++ app/src/main/java/pro/progr/owlgame/domain/GrantBuildingToAnimalUseCase.kt
@@ -1,15 +1,18 @@
 package pro.progr.owlgame.domain
 
+import pro.progr.owlgame.data.preferences.OwlPreferences
 import pro.progr.owlgame.data.repository.AnimalsRepository
 import pro.progr.owlgame.data.repository.BuildingsRepository
 import javax.inject.Inject
 
 class GrantBuildingToAnimalUseCase @Inject constructor(
     val buildingsRepository: BuildingsRepository,
-    val animalsRepository: AnimalsRepository
+    val animalsRepository: AnimalsRepository,
+    val owlPreferences: OwlPreferences
 ) {
     operator fun invoke(buildingId: String, animalId: String) {
         buildingsRepository.updateAnimalId(buildingId, animalId)
         animalsRepository.setPet(animalId)
+        owlPreferences.clearAnimalDayAndId()
     }
 }
