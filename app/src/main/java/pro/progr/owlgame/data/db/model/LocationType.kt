13632ad1f26170698dabb364efd4e9dcc5e43443
Anastasiia Makarenko <irishmoth@yandex.ru>
2026-05-27 23:18:11 +0300

    LocationType

---
+++ app/src/main/java/pro/progr/owlgame/data/db/model/LocationType.kt
@@ -0,0 +1,13 @@
+package pro.progr.owlgame.data.db.model
+
+enum class LocationType {
+    PARK,
+    MONUMENT,
+    COLUMN,
+    PAVILION,
+    CAVE,
+    FOUNTAIN,
+    PALM_TREE,
+    WATERFALL,
+    RUINS
+}
