7b2994caecb88c140c02c0551263e2a0c9fb1f9c
Anastasiia Makarenko <irishmoth@yandex.ru>
2026-05-29 16:19:24 +0300

    Иконки для разных LocationType привязываю к типам (получение иконки по типу)

--- app/src/main/java/pro/progr/owlgame/presentation/ui/mapicon/drawableRecourses.kt
+++ app/src/main/java/pro/progr/owlgame/presentation/ui/mapicon/drawableRecourses.kt
@@ -2,6 +2,7 @@ package pro.progr.owlgame.presentation.ui.mapicon
 
 import androidx.annotation.DrawableRes
 import pro.progr.owlgame.R
+import pro.progr.owlgame.data.db.model.LocationType
 import pro.progr.owlgame.domain.model.BuildingType
 import pro.progr.owlgame.domain.model.ItemType
 
@@ -20,6 +21,20 @@ fun gardenItemIconRes(type: ItemType): Int = when (type) {
     ItemType.ANIMAL_HOUSE -> R.drawable.ic_animal_house
     ItemType.NEST -> R.drawable.ic_nest
     ItemType.MUSHROOM -> R.drawable.ic_mushroom
+}
+
+@DrawableRes
+fun locationIconRes(type: LocationType): Int = when (type) {
+    LocationType.WATER_ANOMALY -> R.drawable.ic_water_anomaly
+    LocationType.FOUNTAIN -> R.drawable.ic_fountain
+    LocationType.WATERFALL -> R.drawable.ic_waterfall
+    LocationType.LANDMARK -> R.drawable.ic_landmark
+    LocationType.MONUMENT -> R.drawable.ic_monument
+    LocationType.CAVE -> R.drawable.ic_cave
+    LocationType.PARK -> R.drawable.ic_park
+    LocationType.PAVILION -> R.drawable.ic_pavilion
+    LocationType.RESORT -> R.drawable.ic_resort
+    LocationType.RUINS -> R.drawable.ic_ruins
 }
 
 @DrawableRes
