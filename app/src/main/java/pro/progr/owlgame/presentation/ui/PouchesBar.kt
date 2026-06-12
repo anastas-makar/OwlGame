21c1320aa165b4f9c7872effd80c3b34d74919eb
Anastasiia Makarenko <irishmoth@yandex.ru>
2026-06-12 16:24:44 +0300

    продолжение локализации мешочка

--- app/src/main/java/pro/progr/owlgame/presentation/ui/PouchesBar.kt
+++ app/src/main/java/pro/progr/owlgame/presentation/ui/PouchesBar.kt
@@ -8,7 +8,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import pro.progr.owlgame.R
 import pro.progr.owlgame.presentation.viewmodel.PouchesViewModel
 
 @Composable
@@ -17,12 +19,12 @@ fun PouchesBar(backToMain : () -> Unit,
     TopAppBar(
         title = {
             Text(text = if (pouchesViewModel.isPouchSelected.value)
-                    "Вы открыли мешочек"
-                else "Выберите мешочек")
+                stringResource(R.string.pouch_opened)
+                else stringResource(R.string.choose_pouch))
         },
         navigationIcon = {
             IconButton(onClick = { backToMain() }) {
-                Icon(Icons.AutoMirrored.Filled.ExitToApp, contentDescription = "Назад")
+                Icon(Icons.AutoMirrored.Filled.ExitToApp, contentDescription = stringResource(R.string.back))
             }
         },
         backgroundColor = Color.Transparent,
