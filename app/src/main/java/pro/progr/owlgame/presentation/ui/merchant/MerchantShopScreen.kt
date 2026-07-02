7872741dcf2a86213bec76c912a3c0829c7ad673
Anastasiia Makarenko <irishmoth@yandex.ru>
2026-07-02 08:38:12 +0300

    цвет кнопки "назад"

--- app/src/main/java/pro/progr/owlgame/presentation/ui/merchant/MerchantShopScreen.kt
+++ app/src/main/java/pro/progr/owlgame/presentation/ui/merchant/MerchantShopScreen.kt
@@ -18,7 +18,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import pro.progr.owlgame.R
 import pro.progr.owlgame.presentation.ui.model.merchant.MerchantItemKey
 import pro.progr.owlgame.presentation.ui.model.merchant.MerchantShopItemUi
@@ -58,6 +60,8 @@ fun MerchantShopScreen(
                             ?: stringResource(R.string.merchant_shop_default_title)
                     )
                 },
+                backgroundColor = Color.Transparent,
+                elevation = 0.dp,
                 navigationIcon = {
                     IconButton(onClick = onBack) {
                         Icon(
